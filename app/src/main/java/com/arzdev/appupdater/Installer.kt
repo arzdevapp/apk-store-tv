d4be7b5d02d8e8cfe9ca082d4fdb95c5535c82ff
Kronos <kronos.arz@gmail.com>
2026-08-29 18:29:13 -0700

    Fix: add missing PackageManager import in Installer.kt

--- app/src/main/java/com/arzdev/appupdater/Installer.kt
+++ app/src/main/java/com/arzdev/appupdater/Installer.kt
@@ -3,6 +3,7 @@ package com.arzdev.appupdater
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageInstaller
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Environment
 import android.provider.Settings
